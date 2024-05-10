a348dcaecca9eed296faea2fbfa919bfecb4c1ee
Ahmad Wirayuda <wirayudha@impstudio.co.id>
2024-05-10 16:11:07 +0700

    start median

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    // vector median
+    let vector = vec![1, 2, 4, 5, 6, 7, 1, 4, 6, 8, 3, 4, 7, 3, 5, 4, 1, 6, 8, 9, 5];
+    median(vector);
+}
+
+fn median(v: Vec<i32>)->i32{
+    for i in &v {
+        println!("{:?}", i);
+        // logic for median
+    };
+    5
 }
